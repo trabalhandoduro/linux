caa4ceff3708b50f373926cb9286c419aefd50fe
trabalhandoduro <trabalhandoduro@gmail.com>
2023-07-03 10:47:09 -0400

    Add files via upload

---
+++ 02.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Instala a ferramenta libmime-tools-perl
+sudo apt-get install libmime-tools-perl -y
+
+# Instala a ferramenta make
+sudo apt-get install make -y
+
+# Instala a biblioteca libmime-lite-perl
+sudo apt-get install libmime-lite-perl -y
+
+# Instala a biblioteca libmime-base64-urlsafe-perl
+sudo apt-get install libmime-base64-urlsafe-perl -y
+
+# Instala o pacote mime-support
+sudo apt-get install -y mime-support
